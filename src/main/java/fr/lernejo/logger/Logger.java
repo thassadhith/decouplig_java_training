435b4d93ed8afa2d791edbb41b07b1b21b0dcf01
thassadhith <sissilarbi19@gmail.com>
2021-12-07 11:06:16 +0100

    partlogger

---
+++ src/main/java/fr/lernejo/logger/Logger.java
@@ -0,0 +1,5 @@
+  package fr.lernejo.logger; // package
+
+    public interface Logger { // interface Logger
+        public void log(String message);//créer une interface Logger avec une seule méthode abstraite
+    }
---
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -0,0 +1,13 @@
+package fr.lernejo.logger;
+
+
+
+    public class LoggerFactory{
+
+
+
+        public static ConsoleLogger getLogger(String name){
+            return new ConsoleLogger();
+        }
+    }
+
